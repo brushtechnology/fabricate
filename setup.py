dcee1674263d846e6c71513f04323f90b22623d6
Chris Coetzee <chriscz93@gmail.com>
2017-05-03 13:49:17 +0200

    Prepare setup.py for PyPI distribution

--- setup.py
+++ setup.py
@@ -2,23 +2,31 @@ from setuptools import setup
 
 from fabricate import __version__
 
-readme = open('README.md').read()
+# see https://github.com/pypa/pypi-legacy/issues/148
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
 
 setup(
     name='fabricate',
     version=__version__,
     description='The better build tool. Finds dependencies automatically for any language.',
-    long_description=readme,
-    author='Brush Technology',
+    long_description=long_description,
+    license='New BSD License',
+    maintainer='Chris Coetzee',
+    maintainer_email='chriscz93@gmail.com',
     url='https://github.com/SimonAlfie/fabricate/',
     py_modules=['fabricate'],
 
     extras_require=dict(
-        build=['twine', 'wheel', 'setuptools-git'],
+        build=['twine', 'wheel', 'setuptools-git', 'pypandoc'],
     ),
 
     keywords='fabricate make python build',
     classifiers=[
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development :: Build Tools',
         'Programming Language :: Python :: 2.6',
@@ -29,6 +37,12 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    platforms=[
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux'
+    ]
 )
 
 
